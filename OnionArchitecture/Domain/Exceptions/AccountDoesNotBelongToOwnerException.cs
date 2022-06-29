99ce4d2a70c443a19cc69c3e5d92aaa3a42bd2da
Shane Monck <shane@constructivesoftware.com.au>
2022-06-29 16:46:12 +0800

    Initial Commit

---
+++ OnionArchitecture/Domain/Exceptions/AccountDoesNotBelongToOwnerException.cs
@@ -0,0 +1,10 @@
+namespace Domain.Exceptions
+{
+    public sealed class AccountDoesNotBelongToOwnerException: BadRequestException
+    {
+        public AccountDoesNotBelongToOwnerException(Guid ownerId, Guid accountId):
+            base($"The account with the identifier {accountId} does not belong to the owner with the identifier {ownerId}")
+        {
+        }
+    }
+}
